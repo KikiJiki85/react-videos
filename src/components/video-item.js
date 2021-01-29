40b6303d7b8c6a6645967fd31b0e58fec3bdbdce
Aleksey Vaskin <alzor85@gmail.com>
2021-01-29 15:42:44 +0300

    Adding some css

--- src/components/video-item.js
+++ src/components/video-item.js
@@ -1,10 +1,14 @@
+import './video-item.css';
 import React from 'react';
 
 const VideoItem = ({ video }) => {
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} alt="pic"/>
-            {video.snippet.title}
+        <div className="item video-item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="pic"/>
+            <div className="content">
+                <div className="header">{video.snippet.title}</div> 
+            </div>            
+            
         </div>
     );
 }
