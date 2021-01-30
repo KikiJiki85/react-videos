2b16d0389c8dc3f001842325bde8cdd31564fd3f
Aleksey Vaskin <alzor85@gmail.com>
2021-01-30 09:36:33 +0300

    Adding video-detail component with conditional rendering

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import VideoItem from './video-item';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} key={video.id.videoId}/>
+        return <VideoItem video={video} key={video.id.videoId} onVideoSelect={onVideoSelect}/>
     });
     
     return (
